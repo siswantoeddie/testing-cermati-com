9c97d455c594d1816c1fa7b9d1cbf311feff0814
edisis <siswantoeddie@gmail.com>
2022-10-09 19:41:27 +0700

    Create pom.xml

---
+++ cermati.com/src/main/java/cermati/com/cermati/com/App.java
@@ -0,0 +1,13 @@
+package cermati.com.cermati.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
